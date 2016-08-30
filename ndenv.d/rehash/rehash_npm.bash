016195820372c28d6f87d2f647ea0d15bed05601
Zak B. Elep <zakame@zakame.net>
2016-08-30 15:58:49 +0800

    Automatically call `ndenv rehash` on calling npm
    Adapted from https://github.com/tokuhirom/plenv/issues/30

---
+++ ndenv.d/rehash/rehash_npm.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+NPM_SHIM_PATH="$SHIM_PATH/npm"
+
+command -p cat > "$NPM_SHIM_PATH" <<SH
+#!/usr/bin/env bash
+set -e
+[ -n "\$NDENV_DEBUG" ] && set -x
+
+program="\${0##*/}"
+export NDENV_ROOT="/home/zakame/.ndenv"
+"$(command -v ndenv)" exec "\$program" "\$@"
+rc=\$?
+for arg in \$W
+do
+  case \$arg in
+    '-h'|'--help'|'-v'|'--version'|'-l')
+      exit \$rc
+    ;;
+  esac
+done
+"$(command -v ndenv)" rehash
+exit \$rc
+SH
+
+command -p chmod +x "$NPM_SHIM_PATH"
